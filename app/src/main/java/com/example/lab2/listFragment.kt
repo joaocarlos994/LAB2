e7e64bb750f21c3f38a5016ea6d1a7897c62fd09
joaocarlos994 <joaocarlosralha@ipvc.pt>
2024-03-30 22:33:29 +0000

    aa

---
+++ app/src/main/java/com/example/lab2/listFragment.kt
@@ -0,0 +1,15 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.lab2.R
+
+class ListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+}
